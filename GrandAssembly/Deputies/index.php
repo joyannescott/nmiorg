2abba5151a4537af0831a1e5142584bc2071d982
Joy Scott <joyfitz@mac.com>
2014-05-15 14:17:35 -0700

    Fixed postit on front page

--- GrandAssembly/Deputies/index.php
+++ GrandAssembly/Deputies/index.php
@@ -7,8 +7,8 @@
    include( INC . 'sidebar.php');
   ?>
 
-<div id="mainContent">
-<div class="mozilla"></div>
+  <div id="mainContent">
+  	  <div class="mozilla"></div>
 <h1>Grand Deputies</h1>
   	<center>
   	<div> <span id="go">Mrs. Joy Scott ~</span><span id="office"> PGO</span></div>
@@ -18,9 +18,9 @@
   	<div> <span id="go">Miss Keilyn Wright ~</span><span id="office"> PGWA</span></div>
   	<div> <span id="go">Miss Amber Deveraux ~</span><span id="office"> PGWA</span></div>
 </center>
-<div class="mozilla"></div>
-<div class="mozilla"></div>
-<div class="mozilla"></div>
+  <div class="mozilla"></div>
+    <div class="mozilla"></div>
+      <div class="mozilla"></div>
     <!-- end #mainContent -->
     </div>
 <?php 
